cd9cbcfa41adc79fdf319b248354a0d7c88a537a
wentangliang <1015764270@qq.com>
2018-09-20 23:36:26 -0400

    Site updated: 2018-09-20 23:36:25

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Sep 20 2018 23:31:40 GMT-0400 (GMT-04:00)
+// build time:Thu Sep 20 2018 23:36:16 GMT-0400 (GMT-04:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
