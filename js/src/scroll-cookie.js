70d57361e98a05502dc73ce3112e8ba5284853f3
wentangliang <1015764270@qq.com>
2018-11-26 22:49:40 -0500

    Site updated: 2018-11-26 22:49:39

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Nov 20 2018 22:08:39 GMT-0500 (GMT-05:00)
+// build time:Mon Nov 26 2018 22:49:28 GMT-0500 (GMT-05:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
